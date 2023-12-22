80fed662984a22665c6a4d633d31e82c89e227ab
IlyasseLaaraj <laarajilyasse001@gmail.com>
2023-12-22 21:40:47 +0100

    Done js for featuredNews and Joc + added a btn for DarkMode that i ll delete later

--- javascript/components/Ilyasse-Joc&Featured/Joc&FeaturedIlyasse.js
+++ javascript/components/Ilyasse-Joc&Featured/Joc&FeaturedIlyasse.js
@@ -2,6 +2,13 @@ const $jocFirstDiv = document.querySelector("#joc_firstDiv");
 const $jocSecondDiv = document.querySelector("#joc_secondDiv");
 const $jocFirstSpan = document.querySelector("#joc_firstSpan");
 const $jocSecondSpan = document.querySelector("#joc_secondSpan");
+const $featuredNewsLeftBtn = document.querySelector("#featuredNews_leftBtn");
+const $featuredNewsRightBtn = document.querySelector("#featuredNews_rightBtn");
+const $featuredNewsSliderContainer = document.querySelector(
+  "#featuredNews_sliderContainer"
+);
+const $html = document.querySelector("html");
+const $dmBtn = document.querySelector("#darkModeBtn");
 
 const addEventListners = () => {
   $jocFirstSpan.addEventListener("click", () => {
@@ -12,6 +19,24 @@ const addEventListners = () => {
   $jocSecondSpan.addEventListener("click", () => {
     $jocFirstDiv.classList.add("hidden");
     $jocSecondDiv.classList.remove("hidden");
+  });
+
+  $featuredNewsLeftBtn.addEventListener("click", () => {
+    $featuredNewsSliderContainer.scrollLeft -=
+      $featuredNewsSliderContainer.clientWidth;
+  });
+
+  $featuredNewsRightBtn.addEventListener("click", () => {
+    $featuredNewsSliderContainer.scrollLeft +=
+      $featuredNewsSliderContainer.clientWidth;
+  });
+
+  $dmBtn.addEventListener("click", () => {
+    if ($html.classList.contains("dark")) {
+      $html.classList.remove("dark");
+    } else {
+      $html.classList.add("dark");
+    }
   });
 };
 
