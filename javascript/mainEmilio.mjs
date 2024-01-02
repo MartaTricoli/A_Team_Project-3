a0bcb8ed01990f9cd07a3b60e045e4f352a279b9
Emilio <lemigliot@gmail.com>
2024-01-02 19:13:32 +0100

    finito js, aggiunger dark mode

---
+++ javascript/mainEmilio.mjs
@@ -0,0 +1,108 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    //ANCHOR - swipper
+    const spans = document.querySelectorAll('.z-6 span');
+    const containerSpan = document.getElementById('span_container')
+    spans[0].classList.add('active');
+
+    const cleanSpan = () => {
+        spans.forEach((otherSpan) => {
+            otherSpan.classList.add('opacity-50');
+            otherSpan.classList.remove('active');
+
+        });
+
+    }
+
+    const cleanSwipper = () => {
+        const allSwipper = document.querySelectorAll('.swipper')
+        allSwipper.forEach((swipper) => {
+            swipper.classList.add('hidden')
+        })
+    }
+
+    containerSpan.addEventListener('click', function (event) {
+        const element = event.target
+        const idSwipper = element.getAttribute('reference');
+
+        cleanSpan()
+        element.classList.remove('opacity-50')
+        element.classList.add('active')
+
+        const swipperSelected = document.getElementById(idSwipper)
+        cleanSwipper()
+
+        swipperSelected.classList.remove('hidden')
+
+
+    })
+
+    setInterval(() => {
+
+        const activeElement = containerSpan.querySelector('.active');
+
+        const nextElement = activeElement.nextElementSibling;
+        const firstElement = spans[0];
+        const idSwipper = nextElement ? nextElement.getAttribute('reference') : firstElement.getAttribute('reference');
+        const swipperSelected = document.getElementById(idSwipper);
+        cleanSwipper()
+
+        swipperSelected.classList.remove('hidden')
+
+        if (nextElement) {
+            cleanSpan()
+
+            nextElement.classList.remove('opacity-50');
+            nextElement.classList.add('active');
+        } else {
+            cleanSpan()
+            firstElement.classList.remove('opacity-50');
+            firstElement.classList.add('active');
+        }
+
+
+
+    }, 10000);
+
+    //ANCHOR - countdown
+
+    const cdContainer = document.querySelector('#cd_container')
+    const days = cdContainer.querySelector('#days')
+    const hour = cdContainer.querySelector('#hour')
+    const minutes = cdContainer.querySelector('#minutes')
+    const seconds = cdContainer.querySelector('#seconds')
+
+    const cdTiming = () => {
+        const countDownDate = new Date("Jan 30, 2024 15:37:25").getTime();
+        const now = new Date().getTime();
+
+        const distance = countDownDate - now;
+
+        const daysTime = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hoursTime = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutesTime = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const secondsTime = Math.floor((distance % (1000 * 60)) / 1000);
+
+        days.innerHTML = daysTime
+        hour.innerHTML = hoursTime
+        minutes.innerHTML = minutesTime
+        seconds.innerHTML = secondsTime
+    }
+
+
+    cdTiming()
+
+
+    const x = setInterval(function () {
+
+        cdTiming()
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
+
+
+});
