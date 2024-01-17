ef1d08af262593e031371aa90a3fc47538849359
TesEzzo <francesco.barranca00@gmail.com>
2024-01-17 13:38:47 +0100

    navbar finished

--- javascript/components/navbar/navbar.js
+++ javascript/components/navbar/navbar.js
@@ -1,261 +1,198 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //desktop
-  const $hoverTrade = document.querySelector("#nTrade");
-  const $hoverEarn = document.querySelector("#nEarn");
-  const $hoverGame = document.querySelector("#nGame");
-  const $hoverNFT = document.querySelector("#nNFT");
-  const $hoverMenu = document.querySelector("#nMenu");
-  const $hoverNetwork = document.querySelector("#nNetwork");
-  const $menuTrade = document.querySelector("#nmTrade");
-  const $menuEarn = document.querySelector("#nmEarn");
-  const $menuGame = document.querySelector("#nmGame");
-  const $menuNFT = document.querySelector("#nmNFT");
-  const $menuMenu = document.querySelector("#nmMenu");
-  const $menuNetwork = document.querySelector("#nmNetwork");
-  //mobile
-  const $hoverEarnMB = document.querySelector("#nmbEarn");
-  const $hoverGameMB = document.querySelector("#nmbGame");
-  const $hoverNFTMB = document.querySelector("#nmbNFT");
-  const $hoverMenuMB = document.querySelector("#nmbMenu");
-  const $menuEarnMB = document.querySelector("#nmbmEarn");
-  const $menuGameMB = document.querySelector("#nmbmGame");
-  const $menuNFTMB = document.querySelector("#nmbmNFT");
-  const $menuMenuMB = document.querySelector("#nmbmMenu");
-  
-  const setListeners = () => {
-  //desktop
-    const trade = () => {
-      $hoverTrade.addEventListener("mouseenter", () => {
-        $menuTrade.classList.remove("hidden");
+const $body = document.querySelector("body")
+const $scrollButton = document.querySelector("#btn-scroll");
+const $closeWarningButton = document.querySelectorAll(".close-warning");
+const $warningDiv = document.querySelectorAll(".warning");
+const $tranButton = document.querySelectorAll(".btn-tran");
+const $settingButton = document.querySelector("#setting_btn");
+const $closeSettingButton = document.querySelector("#close_setting_btn");
+const $containerBelowS = document.querySelector("#container__below_S");
+const $containerBelowSbg = document.querySelector("#container__below_S-bg");
+const $header = document.querySelector("#header")
+
+
+  $settingButton.addEventListener("click", () => {
+        $containerBelowS.classList.add("connect_wallet_appear");
+        $body.classList.add("overflow-y-hidden")
       });
-      $hoverTrade.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverTrade.contains(event.relatedTarget) &&
-          !$menuTrade.contains(event.relatedTarget)
-        ) {
-          $menuTrade.classList.add("hidden");
-        }
+      
+      disappearItemFade = (item) => {
+        item.animate({ opacity: "0" }, 400, () => {
+          item.classList.remove("connect_wallet_appear");
+        });
+      },
+      
+      
+      $containerBelowSbg.addEventListener("click", () => {
+        disappearItemFade($containerBelowS);
+        setTimeout(() => {
+          $containerBelowS.classList.remove("connect_wallet_appear")
+          $body.classList.remove("overflow-y-hidden")
+        }, 300)
       });
-      $menuTrade.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverTrade.contains(event.relatedTarget) &&
-          !$menuTrade.contains(event.relatedTarget)
-        ) {
-          $menuTrade.classList.add("hidden");
-        }
-      });
-    };
-  
-    const earn = () => {
-      $hoverEarn.addEventListener("mouseenter", () => {
-        $menuEarn.classList.remove("hidden");
-      });
-      $hoverEarn.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverEarn.contains(event.relatedTarget) &&
-          !$menuEarn.contains(event.relatedTarget)
-        ) {
-          $menuEarn.classList.add("hidden");
-        }
-      });
-      $menuEarn.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverEarn.contains(event.relatedTarget) &&
-          !$menuEarn.contains(event.relatedTarget)
-        ) {
-          $menuEarn.classList.add("hidden");
-        }
-      });
-    };
-  
-    const game = () => {
-      $hoverGame.addEventListener("mouseenter", () => {
-        $menuGame.classList.remove("hidden");
-      });
-      $hoverGame.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverGame.contains(event.relatedTarget) &&
-          !$menuGame.contains(event.relatedTarget)
-        ) {
-          $menuGame.classList.add("hidden");
-        }
-      });
-      $menuGame.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverGame.contains(event.relatedTarget) &&
-          !$menuGame.contains(event.relatedTarget)
-        ) {
-          $menuGame.classList.add("hidden");
-        }
-      });
-    };
-  
-    const nft = () => {
-      $hoverNFT.addEventListener("mouseenter", () => {
-        $menuNFT.classList.remove("hidden");
-      });
-      $hoverNFT.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverNFT.contains(event.relatedTarget) &&
-          !$menuNFT.contains(event.relatedTarget)
-        ) {
-          $menuNFT.classList.add("hidden");
-        }
-      });
-      $menuNFT.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverNFT.contains(event.relatedTarget) &&
-          !$menuNFT.contains(event.relatedTarget)
-        ) {
-          $menuNFT.classList.add("hidden");
-        }
-      });
-    };
-  
-    const menu = () => {
-      $hoverMenu.addEventListener("mouseenter", () => {
-        $menuMenu.classList.remove("hidden");
-      });
-      $hoverMenu.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverMenu.contains(event.relatedTarget) &&
-          !$menuMenu.contains(event.relatedTarget)
-        ) {
-          $menuMenu.classList.add("hidden");
-        }
-      });
-      $menuMenu.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverMenu.contains(event.relatedTarget) &&
-          !$menuMenu.contains(event.relatedTarget)
-        ) {
-          $menuMenu.classList.add("hidden");
-        }
-      });
-    };
-  
-    const network = () => {
-      $hoverNetwork.addEventListener("mouseenter", () => {
-        $menuNetwork.classList.remove("hidden");
-      });
-      $hoverNetwork.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverNetwork.contains(event.relatedTarget) &&
-          !$menuNetwork.contains(event.relatedTarget)
-        ) {
-          $menuNetwork.classList.add("hidden");
-        }
-      });
-      $menuNetwork.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverNetwork.contains(event.relatedTarget) &&
-          !$menuNetwork.contains(event.relatedTarget)
-        ) {
-          $menuNetwork.classList.add("hidden");
-        }
-      });
-    };
-//mobile
-    const mbEarn = () => {
-      $hoverEarnMB.addEventListener("mouseenter", () => {
-        $menuEarnMB.classList.remove("hidden");
-      });
-      $hoverEarnMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverEarnMB.contains(event.relatedTarget) &&
-          !$menuEarnMB.contains(event.relatedTarget)
-        ) {
-          $menuEarnMB.classList.add("hidden");
-        }
-      });
-      $menuEarnMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverEarnMB.contains(event.relatedTarget) &&
-          !$menuEarnMB.contains(event.relatedTarget)
-        ) {
-          $menuEarnMB.classList.add("hidden");
-        }
-      });
-    };
-  
-    const mbGame = () => {
-      $hoverGameMB.addEventListener("mouseenter", () => {
-        $menuGameMB.classList.remove("hidden");
-      });
-      $hoverGameMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverGameMB.contains(event.relatedTarget) &&
-          !$menuGameMB.contains(event.relatedTarget)
-        ) {
-          $menuGameMB.classList.add("hidden");
-        }
-      });
-      $menuGameMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverGameMB.contains(event.relatedTarget) &&
-          !$menuGameMB.contains(event.relatedTarget)
-        ) {
-          $menuGameMB.classList.add("hidden");
-        }
-      });
-    };
-  
-    const mbNft = () => {
-      $hoverNFTMB.addEventListener("mouseenter", () => {
-        $menuNFTMB.classList.remove("hidden");
-      });
-      $hoverNFTMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverNFTMB.contains(event.relatedTarget) &&
-          !$menuNFTMB.contains(event.relatedTarget)
-        ) {
-          $menuNFTMB.classList.add("hidden");
-        }
-      });
-      $menuNFTMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverNFTMB.contains(event.relatedTarget) &&
-          !$menuNFTMB.contains(event.relatedTarget)
-        ) {
-          $menuNFTMB.classList.add("hidden");
-        }
-      });
-    };
-  
-    const mbMenu = () => {
-      $hoverMenuMB.addEventListener("mouseenter", () => {
-        $menuMenuMB.classList.remove("hidden");
-      });
-      $hoverMenuMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverMenuMB.contains(event.relatedTarget) &&
-          !$menuMenuMB.contains(event.relatedTarget)
-        ) {
-          $menuMenuMB.classList.add("hidden");
-        }
-      });
-      $menuMenuMB.addEventListener("mouseout", (event) => {
-        if (
-          !$hoverMenuMB.contains(event.relatedTarget) &&
-          !$menuMenuMB.contains(event.relatedTarget)
-        ) {
-          $menuMenuMB.classList.add("hidden");
-        }
-      });
-    };
-  
-//desktop
-    trade();
-    earn();
-    game();
-    nft();
-    menu();
-    network();
-//mobile
-    mbEarn();
-    mbGame();
-    mbNft();
-    mbMenu();
-  };
-  setListeners();
+      $closeSettingButton.addEventListener("click", () => {
+        disappearItemFade($containerBelowS);
+        setTimeout(() => {
+    $body.classList.remove("overflow-y-hidden")
+    $containerBelowS.classList.remove("connect_wallet_appear")
+}, 300)
 });
+
+
+const setListeners = () => {
+  const scrollTop = () => {
+    window.addEventListener('scroll', () => {
+      var scrollY = window.scrollY;
+      var scrollThreshold = 300;
+  
+      if (scrollY > scrollThreshold) {
+        $scrollButton.classList.remove("hidden");
+      } else {
+        $scrollButton.classList.add("hidden");
+      }
+    });
+
+    $scrollButton.addEventListener("click", () => {
+      window.scrollTo(0, 0);
+    });
+  };
+
+  const closingWarning = () => {
+    $closeWarningButton.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        // $header.classList.remove("mt-31.5");
+        // $header.classList.add("mt-[54px]");
+        $warningDiv.forEach((div) => {
+          div.classList.add("hidden");
+          div.classList.remove("max-mq3:flex");
+        });
+      });
+    });
+  };
+
+  const buttonSelection = () => {
+    const changeColor = (clickedIndex) => {
+      $tranButton.forEach((button, index) => {
+        if (index + 1 === clickedIndex) {
+          button.classList.remove('btn_settingNOT');
+          button.classList.add('btn_blue_setting');
+        } else {
+          button.classList.remove('btn_blue_setting');
+          button.classList.add('btn_settingNOT');
+        }
+      });
+    };
+
+    $tranButton.forEach((button, index) => {
+      button.addEventListener('click', () => changeColor(index + 1));
+    });
+  };
+
+  const dropdownNet = () => {
+    const selectMap = {
+      "BNB Chain": {
+        name: "BNB Chain",
+        nameM:"BNB",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/56.png",
+      },
+      Ethereum: {
+        name: "Ethereum",
+        nameM:"ETH",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/1.png",
+      },
+      "Polygon zkEVM": {
+        name: "Polygon zkEVM",
+        nameM:"POL",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/1101.png",
+      },
+      "zkSync Era": {
+        name: "zkSync Era",
+        nameM:"zkSync",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/324.png",
+      },
+      "Arbitrum One": {
+        name: "Arbitrum One",
+        nameM:"ARB",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/42161.png",
+      },
+      Linea: {
+        name: "Linea",
+        nameM: "Linea",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/59144.png",
+      },
+      Base: {
+        name: "Base",
+        nameM: "Base",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/8453.png",
+      },
+      opBNB: {
+        name: "opBNB",
+        nameM: "opBNB",
+        imgSrc: "https://assets.pancakeswap.finance/web/chains/204.png",
+      },
+      Aptos: {
+        name: "Aptos",
+        nameM: "Aptos",
+        imgSrc: "https://aptos.pancakeswap.finance/images/apt.png",
+      },
+    };
+    
+    const $menuItemList = document.querySelector("#menu-item-list");
+    const $selectedValue = document.querySelector("#selected-value");
+    
+    const renderMenuList = () => {
+      const last =Object.keys(selectMap).pop()
+      console.log(last);
+      console.log(selectMap);
+      Object.entries(selectMap).forEach(([key, item], index) => {
+        $menuItemList.innerHTML += `
+                  <li data-id="${key}" class="${last === key ? "rounded-b-lg" : ""} flex items-center h-12 w-full px-4 hover:bg-color_nav_hover dark:hover:bg-color_darkM_hover text-color_text dark:text-white group/element flex gap-[10px] cursor-pointer mb-[5px] menu-item${index == 0 ? " active" : ""}">
+                        <img data-id="${key}" class="w-6 h-6 menu-item" src="${item.imgSrc}" />
+                        <span data-id="${key}" class="menu-item">${item.name}</span>
+                  </li>`;
+      });
+    };
+    
+    const setListeners = () => {
+      document.addEventListener("click", (event) => {
+        if (event.target.classList.contains("menu-item")) {
+          const id = event.target.dataset.id;
+          const media = window.matchMedia("(max-width: 1075px)")
+
+    
+          $selectedValue.innerHTML = `
+              <img class="w-8 h-8 mr-3" src="${selectMap[id].imgSrc}" />
+            <span class="text-color_text dark:text-white">${media.matches ? selectMap[id].nameM : selectMap[id].name}</span>
+            <svg viewBox="0 0 24 24" color="text" width="24px" xmlns="http://www.w3.org/2000/svg"
+            class="sc-bcPKhP cvmRst h-6 w-6 ml-1,5 mr-2 dark:fill-white">
+            <path
+              d="M8.11997 9.29006L12 13.1701L15.88 9.29006C16.27 8.90006 16.9 8.90006 17.29 9.29006C17.68 9.68006 17.68 10.3101 17.29 10.7001L12.7 15.2901C12.31 15.6801 11.68 15.6801 11.29 15.2901L6.69997 10.7001C6.30997 10.3101 6.30997 9.68006 6.69997 9.29006C7.08997 8.91006 7.72997 8.90006 8.11997 9.29006Z">
+            </path>
+          </svg>
+          `;
+          
+          $menuItemList.querySelectorAll("li").forEach(item => {
+              if(item.dataset.id != id) {
+                item.classList.remove("active");
+            } else {
+                item.classList.add("active");
+            }
+          })
+        }
+      });
+    };
+    
+    const init = () => {
+      renderMenuList();
+      setListeners();
+    };
+    
+    init();
+    
+  }
+  scrollTop();
+  closingWarning();
+  buttonSelection();
+  dropdownNet()
+};
+
+setListeners();
